fac79c958d74c6aec4558c6d9e7f4f6b55d45f1e
GVHariKowshik <harikowshik06@gmail.com>
2026-06-03 11:25:39 +0530

    project structure ready,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Hari kowshik",
+    author_email="harikowshik06@gmail.com",
+    packages=find_packages()
+)
